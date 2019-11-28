b8c1ba5ca52e0cc8c7c1ac92484cafb98485c930
andrew <andrewjthomsen@yahoo.com>
2019-11-27 23:59:44 -0800

    Made changes to begin incorporating react state and psuedo coded next planned steps

--- src/Components/Roll/Roll.js
+++ src/Components/Roll/Roll.js
@@ -2,15 +2,24 @@ import React, { Component } from 'react';
 import Dice from '../Dice/Dice';
 
 class Roll extends Component {
+    static defaultProps = {
+        sides: ['one', 'two', 'three', 'four', 'five', 'six']
+    };
     constructor (props) {
         super(props);
             this.state = {diceUno: 'one', diceDos: 'one'};
+    }
+
+    roll () {
+        //  Pick 2 new rolls
+        // set state with new rolls
     }
     render () {
         return (
             <div>
                 <Dice face={this.state.diceUno}/>
                 <Dice face={this.state.diceDos}/>
+                <button onClick={this.roll}>Feeling Lucky?</button>
             </div>
         )
     }
