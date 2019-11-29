14c92a992f88f81f5f47055bd755f13093c38e87
andrew <andrewjthomsen@yahoo.com>
2019-11-28 23:02:10 -0800

    Made a change to roll.js file to create a button and added a clickhandler. Then wired up state to change when button is clicked by adding state into constructor.

--- src/Components/Roll/Roll.js
+++ src/Components/Roll/Roll.js
@@ -1,28 +1,37 @@
-import React, { Component } from 'react';
-import Dice from '../Dice/Dice';
+import React, { Component } from "react";
+import Dice from "../Dice/Dice";
 
 class Roll extends Component {
-    static defaultProps = {
-        sides: ['one', 'two', 'three', 'four', 'five', 'six']
-    };
-    constructor (props) {
-        super(props);
-            this.state = {diceUno: 'one', diceDos: 'one'};
-    }
+  static defaultProps = {
+    sides: ["one", "two", "three", "four", "five", "six"]
+  };
+  constructor(props) {
+    super(props);
+    this.state = { diceUno: "one", diceDos: "one" };
+    this.roll = this.roll.bind(this);
+  }
 
-    roll () {
-        //  Pick 2 new rolls
-        // set state with new rolls
-    }
-    render () {
-        return (
-            <div>
-                <Dice face={this.state.diceUno}/>
-                <Dice face={this.state.diceDos}/>
-                <button onClick={this.roll}>Feeling Lucky?</button>
-            </div>
-        )
-    }
+  roll() {
+    //  Pick 2 new rolls
+    // will result in number between 0-last index (5)
+    const newDiceUno = this.props.sides[
+      Math.floor(Math.random() * this.props.sides.length)
+    ];
+    const newDiceDos = this.props.sides[
+      Math.floor(Math.random() * this.props.sides.length)
+    ];
+    // set state with new rolls
+    this.setState({diceUno: newDiceUno, diceDos: newDiceDos})
+  }
+  render() {
+    return (
+      <div>
+        <Dice face={this.state.diceUno} />
+        <Dice face={this.state.diceDos} />
+        <button onClick={this.roll}>Feeling Lucky?</button>
+      </div>
+    );
+  }
 }
 
 export default Roll;
